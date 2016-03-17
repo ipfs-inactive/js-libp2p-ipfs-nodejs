e73023b64658e2e7a613c3c50c7ad5e040a05736
David Dias <daviddias.p@gmail.com>
2016-03-17 00:39:22 +0000

    update tests

--- tests/test-libp2p-ipfs.js
+++ tests/test-libp2p-ipfs.js
@@ -1,7 +1,7 @@
-/* globals describe, it */
+/* eslint-env mocha */
 
 const expect = require('chai').expect
-const libp2p = require('../src')
+const libp2p = require('../src').Node
 const Id = require('peer-id')
 const Peer = require('peer-info')
 const multiaddr = require('multiaddr')
@@ -9,50 +9,35 @@ const multiaddr = require('multiaddr')
 describe('libp2p-ipfs', () => {
   const idA = Id.create()
   const idB = Id.create()
-  const peerA = new Peer(idA, [])
-  const peerB = new Peer(idB, [])
+  const peerA = new Peer(idA)
+  const peerB = new Peer(idB)
 
   var nodeA
   var nodeB
 
   it('prepare node A', (done) => {
-    const options = {
-      peer: peerA,
-      multiaddrs: {
-        tcp: multiaddr('/ip4/127.0.0.1/tcp/8010')
-      }
-    }
+    peerA.multiaddr.add(multiaddr('/ip4/127.0.0.1/tcp/8010'))
 
-    nodeA = libp2p(options)
-    nodeA.listen((err) => {
-      expect(err).to.not.exist
-      done()
-    })
+    nodeA = libp2p(peerA)
+    nodeA.start(done)
   })
-  it('prepare node B', (done) => {
-    const options = {
-      peer: peerB,
-      multiaddrs: {
-        tcp: multiaddr('/ip4/127.0.0.1/tcp/8020')
-      }
-    }
 
-    nodeB = libp2p(options)
-    nodeB.listen((err) => {
-      expect(err).to.not.exist
-      done()
-    })
+  it('prepare node B', (done) => {
+    peerB.multiaddr.add(multiaddr('/ip4/127.0.0.1/tcp/8020'))
+
+    nodeB = libp2p(peerB)
+    nodeB.start(done)
   })
 
   it('B handle /echo/1.0.0', (done) => {
-    nodeB.swarm.handleProtocol('/echo/1.0.0', (conn) => {
+    nodeB.swarm.handle('/echo/1.0.0', (conn) => {
       conn.pipe(conn)
     })
     done()
   })
 
   it('A dial B to speak /echo/1.0.0', (done) => {
-    nodeA.swarm.dial(peerB, {}, '/echo/1.0.0', (err, conn) => {
+    nodeA.swarm.dial(peerB, '/echo/1.0.0', (err, conn) => {
       expect(err).to.not.exist
       done()
     })
