40e5093c61c947e2feb93b5ad7aefa9520105a74
varunagarwal315 <varunagarwal315@gmail.com>
2016-11-18 17:07:07 +0530

    fix: update examples to use async crypto

--- examples/echo/dialer.js
+++ examples/echo/dialer.js
@@ -6,45 +6,63 @@ const PeerInfo = require('peer-info')
 const libp2p = require('../../src')
 const multiaddr = require('multiaddr')
 const pull = require('pull-stream')
+const async = require('async')
+let idListener
 
-// Creation of PeerInfo of Dialer Node (this node)
-const idDialer = PeerId.createFromJSON(require('./peer-id-dialer'))
-const peerDialer = new PeerInfo(idDialer)
-peerDialer.multiaddr.add(multiaddr('/ip4/0.0.0.0/tcp/0'))
-const nodeDialer = new libp2p.Node(peerDialer)
-
-// Creation of PeerInfo of Listener Node
-const idListener = PeerId.createFromJSON(require('./peer-id-listener'))
-const peerListener = new PeerInfo(idListener)
-peerListener.multiaddr.add(multiaddr('/ip4/127.0.0.1/tcp/10333'))
-
-nodeDialer.start((err) => {
-  if (err) {
-    throw err
+async.parallel([
+  (callback) => {
+    PeerId.createFromJSON(require('./peer-id-dialer'), (err, idDialer) => {
+      if (err) {
+        throw err
+      }
+      callback(null, idDialer)
+    })
+  },
+  (callback) => {
+    PeerId.createFromJSON(require('./peer-id-listener'), (err, idListener) => {
+      if (err) {
+        throw err
+      }
+      callback(null, idListener)
+    })
   }
+], (err, ids) => {
+  if (err) throw err
+  const peerDialer = new PeerInfo(ids[0])
+  peerDialer.multiaddr.add(multiaddr('/ip4/0.0.0.0/tcp/0'))
+  const nodeDialer = new libp2p.Node(peerDialer)
 
-  console.log('Dialer ready, listening on:')
-
-  peerListener.multiaddrs.forEach((ma) => {
-    console.log(ma.toString() + '/ipfs/' + idListener.toB58String())
-  })
-
-  nodeDialer.dialByPeerInfo(peerListener, '/echo/1.0.0', (err, conn) => {
+  const peerListener = new PeerInfo(ids[1])
+  idListener = ids[1]
+  peerListener.multiaddr.add(multiaddr('/ip4/127.0.0.1/tcp/10333'))
+  nodeDialer.start((err) => {
     if (err) {
       throw err
     }
-    console.log('nodeA dialed to nodeB on protocol: /echo/1.0.0')
 
-    pull(
-      pull.values(['hey']),
-      conn,
-      pull.through(console.log),
-      pull.collect((err, data) => {
-        if (err) {
-          throw err
-        }
-        console.log('received echo:', data.toString())
-      })
-    )
+    console.log('Dialer ready, listening on:')
+
+    peerListener.multiaddrs.forEach((ma) => {
+      console.log(ma.toString() + '/ipfs/' + idListener.toB58String())
+    })
+
+    nodeDialer.dialByPeerInfo(peerListener, '/echo/1.0.0', (err, conn) => {
+      if (err) {
+        throw err
+      }
+      console.log('nodeA dialed to nodeB on protocol: /echo/1.0.0')
+
+      pull(
+        pull.values(['hey']),
+        conn,
+        pull.through(console.log),
+        pull.collect((err, data) => {
+          if (err) {
+            throw err
+          }
+          console.log('received echo:', data.toString())
+        })
+      )
+    })
   })
 })
--- examples/echo/listener.js
+++ examples/echo/listener.js
@@ -7,43 +7,45 @@ const libp2p = require('../../src')
 const multiaddr = require('multiaddr')
 const pull = require('pull-stream')
 
-const idListener = PeerId.createFromJSON(require('./peer-id-listener'))
-const peerListener = new PeerInfo(idListener)
-peerListener.multiaddr.add(multiaddr('/ip4/0.0.0.0/tcp/10333'))
-const nodeListener = new libp2p.Node(peerListener)
-
-nodeListener.start((err) => {
+PeerId.createFromJSON(require('./peer-id-listener'), (err, idListener) => {
   if (err) {
     throw err
   }
+  const peerListener = new PeerInfo(idListener)
+  peerListener.multiaddr.add(multiaddr('/ip4/0.0.0.0/tcp/10333'))
+  const nodeListener = new libp2p.Node(peerListener)
 
-  nodeListener.swarm.on('peer-mux-established', (peerInfo) => {
-    console.log(peerInfo.id.toB58String())
-  })
+  nodeListener.start((err) => {
+    if (err) {
+      throw err
+    }
 
-  nodeListener.handle('/echo/1.0.0', (protocol, conn) => {
-    pull(
-      conn,
-      conn
-    )
-  })
+    nodeListener.swarm.on('peer-mux-established', (peerInfo) => {
+      console.log(peerInfo.id.toB58String())
+    })
 
-  nodeListener.handle('/hello/1.0.0', (protocol, conn) => {
-    pull(
-      pull.values(['hello', 'world']),
-      conn
-    )
-    setTimeout(() => {
-      console.log('Printing my peerbook')
-      Object
-        .keys(nodeListener.peerBook.getAll())
-        .forEach(console.log)
-    }, 500)
-  })
+    nodeListener.handle('/echo/1.0.0', (protocol, conn) => {
+      pull(
+        conn,
+        conn
+      )
+    })
 
-  console.log('Listener ready, listening on:')
-
-  peerListener.multiaddrs.forEach((ma) => {
-    console.log(ma.toString() + '/ipfs/' + idListener.toB58String())
+    nodeListener.handle('/hello/1.0.0', (protocol, conn) => {
+      pull(
+        pull.values(['hello', 'world']),
+        conn
+      )
+      setTimeout(() => {
+        console.log('Printing my peerbook')
+        Object
+          .keys(nodeListener.peerBook.getAll())
+          .forEach(console.log)
+      }, 500)
+    })
+    console.log('Listener ready, listening on:')
+    peerListener.multiaddrs.forEach((ma) => {
+      console.log(ma.toString() + '/ipfs/' + idListener.toB58String())
+    })
   })
 })
