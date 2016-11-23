721f5e05e5bcae7493e33730daf98d651fc90e75
varun <varunagarwal315@gmail.com>
2016-11-23 12:27:41 +0530

    Chat between terminals done

--- examples/chat/dialer.js
+++ examples/chat/dialer.js
@@ -6,8 +6,10 @@ const PeerInfo = require('peer-info')
 const libp2p = require('../../src')
 const multiaddr = require('multiaddr')
 const pull = require('pull-stream')
-const toPull = require('stream-to-pull-stream')
+// const toPull = require('stream-to-pull-stream')
 const async = require('async')
+const Pushable = require('pull-pushable')
+const p = Pushable()
 let idListener
 
 async.parallel([
@@ -52,30 +54,24 @@ async.parallel([
         throw err
       }
       console.log('nodeA dialed to nodeB on protocol: /chat/1.0.0')
-
-      // With push streams
-      // process.stdin.pipe(conn).pipe(process.stdout) enabled terminal-to-terminal chat
-
-      // pull(
-      //   toPull(process.stdin),
-      //   conn,
-      //   pull.collect((err, data) => {
-      //     if (err) {
-      //       throw err
-      //     }
-      //     console.log(data.toString())
-      //   })
-      // )
-
-      //Produces 'already piped' error, after typing for the second time
-      process.stdin.setEncoding('utf8');
+      // Write operation. Data sent as a buffer
+      pull(
+        p,
+        conn
+      )
+      // Sink, data converted from buffer to utf8 string
+      pull(
+        conn,
+        pull.map((data) => {
+          return data.toString('utf8').replace('\n','')
+        }),
+        pull.drain(console.log)
+      )
+      
+      process.stdin.setEncoding('utf8')
       process.openStdin().on('data', (chunk) => {
         var data = chunk.toString()
-        console.log(data);
-        pull(
-          pull.values(data),
-          conn
-        )
+        p.push(data)
       })
     })
   })
--- examples/chat/listener.js
+++ examples/chat/listener.js
@@ -6,6 +6,8 @@ const PeerInfo = require('peer-info')
 const libp2p = require('../../src')
 const multiaddr = require('multiaddr')
 const pull = require('pull-stream')
+const Pushable = require('pull-pushable')
+const p = Pushable()
 
 PeerId.createFromJSON(require('./peer-id-listener'), (err, idListener) => {
   if (err) {
@@ -25,21 +27,25 @@ PeerId.createFromJSON(require('./peer-id-listener'), (err, idListener) => {
     })
 
     nodeListener.handle('/chat/1.0.0', (protocol, conn) => {
-      pull(
-        conn,
-        pull.collect((err, data) => {
-          if (err) {
-            throw err
-          }
-          console.log(data.toString())
-        })
-      )
 
-      // Just a test
       pull(
-        pull.values(['listener => dialer test']),
+        p,
         conn
       )
+
+      pull(
+        conn,
+        pull.map((data) => {
+          return data.toString('utf8').replace('\n','')
+        }),
+        pull.drain(console.log)
+      )
+
+      process.stdin.setEncoding('utf8')
+      process.openStdin().on('data', (chunk) => {
+        var data = chunk.toString()
+        p.push(data)
+      })
     })
 
     console.log('Listener ready, listening on:')
