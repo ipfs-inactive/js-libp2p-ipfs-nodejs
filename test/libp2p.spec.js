8568783b3091e1e6313efd8ef8a5791b2104a1ff
David Dias <daviddias.p@gmail.com>
2016-12-01 12:24:39 +0000

    fix: sometimes ping is super fast

--- test/libp2p.spec.js
+++ test/libp2p.spec.js
@@ -409,7 +409,7 @@ describe('libp2p-ipfs-nodejs', () => {
     nodeE.pingByPeerInfo(nodeF.peerInfo, (err, ping) => {
       expect(err).to.not.exist
       ping.once('ping', (time) => {
-        expect(time).to.be.above(1)
+        expect(time).to.be.above(0)
         ping.stop()
         done()
       })
