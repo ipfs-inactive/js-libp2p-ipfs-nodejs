c197a62bff853d5dd2334eaef9a41d3ec561bf31
David Dias <daviddias.p@gmail.com>
2017-03-21 09:26:55 +0000

    feat: remove IPFS_BOOTSTRAP flag

--- src/index.js
+++ src/index.js
@@ -67,7 +67,7 @@ class Node extends libp2p {
       modules.discovery.push(mdns)
     }
 
-    if (options.bootstrap && process.env.IPFS_BOOTSTRAP) {
+    if (options.bootstrap) {
       const r = new Railing(options.bootstrap)
       modules.discovery.push(r)
     }
