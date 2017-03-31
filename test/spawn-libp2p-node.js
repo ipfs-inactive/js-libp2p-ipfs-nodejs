a509052ec9032350c221ec3273d02c8db561f8b1
David Dias <daviddias.p@gmail.com>
2017-03-31 16:55:14 +0100

    Advanced peer-book (#91)
    * test: refactor and add moar tests
    * feat: update to latest js-libp2p, peer-info, peer-book

--- test/spawn-libp2p-node.js
+++ test/spawn-libp2p-node.js
@@ -5,18 +5,18 @@
 const Node = require('../src')
 const PeerInfo = require('peer-info')
 const PeerId = require('peer-id')
-const multiaddr = require('multiaddr')
 const pull = require('pull-stream')
 
 const idBak = require('./test-data/test-id.json')
 
 PeerId.createFromJSON(idBak, (err, peerId) => {
-  if (err) { throw err }
+  if (err) {
+    throw err
+  }
 
   const peerInfo = new PeerInfo(peerId)
-  const maddr = multiaddr('/ip4/127.0.0.1/tcp/12345')
 
-  peerInfo.multiaddr.add(maddr)
+  peerInfo.multiaddrs.add('/ip4/127.0.0.1/tcp/12345')
 
   const node = new Node(peerInfo)
 
